2e43d125fa1a5eadf7f7bd1dd3ea2098fb390c7f
Astrojerms <jeremy@jeremys-mac.attlocal.net>
2021-01-03 15:05:26 -0800

    restrict actions from the UI based on user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,8 +18,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    #Hard code user ID until authentication is added - to be removed
-    @article.user = User.first    
+    @article.user = current_user 
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
